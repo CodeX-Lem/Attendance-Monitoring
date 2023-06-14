3d251670796bc3257025d72e351cddd8af9d6cc4
Kert619 <108245652+Kert619@users.noreply.github.com>
2023-06-15 07:10:05 +0800

    Responsive search report

--- resources/views/trainor/reports/index.blade.php
+++ resources/views/trainor/reports/index.blade.php
@@ -4,37 +4,36 @@
 <div class="p-0 p-sm-4">
     <div class="border bg-white p-0 p-sm-3">
         <div class="container-fluid">
-
             <div class="row gx-1 gy-2 gy-md-0 mt-3 align-items-center justify-content-end">
-                <div class="col-md-10">
-                    <form action="" method="GET">
-                        <div class="input-group input-group-sm">
-                            <input type="hidden" class="form-control shadow-none rounded-0" name="entries"
-                                value="{{ request('entries', 5) }}" autocomplete="off">
+                <div class="col-md-6">
+                    <input type="hidden" class="form-control shadow-none rounded-0" name="entries"
+                        value="{{ request('entries', 5) }}" autocomplete="off">
 
-                           <div class="d-flex gap-2 me-3">
-                               <div class="d-flex align-items-center gap-2">
-                                    <label class="form-label mt-1">From</label>
-                                    <input type="date" class="form-control shadow-none rounded-0" name="date-from">
-                               </div>
-                               <div class="d-flex align-items-center gap-2">
-                                    <label class="form-label mt-1">To</label>
-                                    <input type="date" class="form-control shadow-none rounded-0" name="date-to">
-                               </div>
-                           </div>
-
-                            <input type="text" class="form-control shadow-none rounded-0" placeholder="Search any student"
-                                name="search" autocomplete="off" autofocus>
-                            <button type="submit" class="btn btn-success d-inline-flex align-items-center gap-2 rounded-0">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-search" viewBox="0 0 16 16">
-                                    <path
-                                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                                </svg>
-                                <span>Search</span>
-                            </button>
+                    <div class="d-flex flex-column flex-md-row gap-2">
+                        <div class="d-flex align-items-center gap-2 me-3">
+                            <label class="form-label mt-1">From</label>
+                            <input type="date" class="form-control shadow-none rounded-0" name="date-from">
                         </div>
-                    </form>
+                        <div class="d-flex align-items-center gap-2 me-3">
+                            <label class="form-label mt-1">To</label>
+                            <input type="date" class="form-control shadow-none rounded-0" name="date-to">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="input-group input-group-sm">
+                        <input type="text" class="form-control shadow-none rounded-0" placeholder="Search any student"
+                            name="search" autocomplete="off" autofocus>
+                        <button type="submit" class="btn btn-success d-inline-flex align-items-center gap-2 rounded-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-search" viewBox="0 0 16 16">
+                                <path
+                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                            </svg>
+                            <span>Search</span>
+                        </button>
+                    </div>
                 </div>
             </div>
 
@@ -63,7 +62,8 @@
                         @endphp --}}
                     </tbody>
                 </table>
-                {{-- <x-pagination :pageData="$students" route="{{ route('trainor.students.index') }}"></x-pagination> --}}
+                {{-- <x-pagination :pageData="$students" route="{{ route('trainor.students.index') }}"></x-pagination>
+                --}}
             </div>
         </div>
     </div>
